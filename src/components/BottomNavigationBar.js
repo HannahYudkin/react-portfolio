75173d33206f3d5b26b4c5bbe00fc0c0886f67f9
HannahYudkin <hannahyudkin@gmail.com>
2020-06-06 09:39:06 -0400

    Removed scroll to top/added gh pages build.

--- src/components/BottomNavigationBar.js
+++ src/components/BottomNavigationBar.js
@@ -6,7 +6,7 @@ import GitHubIcon from "@material-ui/icons/GitHub";
 import FavoriteIcon from "@material-ui/icons/Favorite";
 import LinkedInIcon from "@material-ui/icons/LinkedIn";
 import DescriptionIcon from "@material-ui/icons/Description";
-import ScrollToTop from './ScrollToTop';
+
 
 //import Link from "@material-ui/core/Link";
 
