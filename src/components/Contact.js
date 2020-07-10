561548a6d5b47e6016846d6f56986d1eca8b0a05
HannahYudkin <hannahyudkin@gmail.com>
2020-07-10 12:32:54 -0400

    Created contact card.

---
+++ src/components/Contact.js
@@ -0,0 +1,29 @@
+import React from "react";
+import Card from "@material-ui/core/Card";
+import { makeStyles } from "@material-ui/core/styles";
+import Typography from "@material-ui/core/Typography";
+
+const useStyles = makeStyles({
+  root: {
+    minWidth: 1200,
+    minHeight: 500,
+  },
+  title: {
+    fontSize: 50,
+    textAlign: "center",
+  },
+  content: {},
+});
+
+export default function Contact() {
+  const classes = useStyles();
+  return (
+    <Card className={classes.root}>
+      <Typography className={classes.title}>Contact Me</Typography>
+      <Typography>Email: HannahYudkin@gmail.com</Typography>
+      <Typography>Phone: (603) 921 - 0450</Typography>
+      <Typography>LinkedIn: www.linkedin.com/in/hannahyudkin/ </Typography>
+      <Typography>Github: https://github.com/HannahYudkin</Typography>
+    </Card>
+  );
+}
