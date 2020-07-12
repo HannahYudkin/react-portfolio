dffb21be85ea9bf393efb63f4c9ff5369016322b
HannahYudkin <hannahyudkin@gmail.com>
2020-07-12 10:54:40 -0400

    Updates

---
+++ precache-manifest.198939b20180076b43eda485fc5f8d05.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31e8be541d4e3bc2a22d17dae78b3323",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4048fca428dd89007d85",
+    "url": "/static/css/main.f829e3c2.chunk.css"
+  },
+  {
+    "revision": "e7484d9c50464ffe0549",
+    "url": "/static/js/2.29c1cd5b.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.29c1cd5b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4048fca428dd89007d85",
+    "url": "/static/js/main.f4cac83d.chunk.js"
+  },
+  {
+    "revision": "967a46d8b67e4e01db1f",
+    "url": "/static/js/runtime-main.7e4b9196.js"
+  },
+  {
+    "revision": "1cbab3467f76bcc32b4dbb80bdac48a8",
+    "url": "/static/media/AboveCurve.1cbab346.png"
+  },
+  {
+    "revision": "cf166077fc7d74b9d7652babc2e38dc9",
+    "url": "/static/media/AboveCurveLogo(base).cf166077.png"
+  },
+  {
+    "revision": "8c980248612b15e01cba5ad3ad19884f",
+    "url": "/static/media/AboveCurveLogo.8c980248.png"
+  },
+  {
+    "revision": "8730dfe4de05a98308aac0ba2e57cf81",
+    "url": "/static/media/BusinessBlueprint.8730dfe4.png"
+  },
+  {
+    "revision": "845a4228821b9b17a1bf2e6140870752",
+    "url": "/static/media/BusinessBlueprintLogo.845a4228.png"
+  },
+  {
+    "revision": "d8de6323c368f990ffbea598412b7d66",
+    "url": "/static/media/FuriendFinder.d8de6323.png"
+  },
+  {
+    "revision": "906a722017abb0284aabbdfed11b7e75",
+    "url": "/static/media/FuriendFinderLogo(base).906a7220.png"
+  },
+  {
+    "revision": "f6bc1a3533380778f0c71304ed8dfbee",
+    "url": "/static/media/FuriendFinderLogo.f6bc1a35.png"
+  },
+  {
+    "revision": "e88de0d5842f50e8380b65869496e5e1",
+    "url": "/static/media/LinkedInPhoto.e88de0d5.jpg"
+  }
+]);
